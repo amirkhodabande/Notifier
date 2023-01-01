6f72838514c21ebcf2f155ac027a036a3356df5d
amir <amirhossein.khodabandeloo@parspack.com>
2023-01-01 15:11:32 +0330

    add a smoke test and configure the phpunit

---
+++ tests/Feature/SmokeTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Amir\Notifier\Tests\Feature;
+
+use Orchestra\Testbench\TestCase;
+
+class SmokeTest extends TestCase
+{
+    /** @test */
+    public function smoke()
+    {
+        $this->assertTrue(true);
+    }
+}
