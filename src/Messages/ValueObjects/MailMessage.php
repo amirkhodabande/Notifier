875e6ed44e66dbc80dc33dc1e82831cc8f146135
amir <amirhossein.khodabandeloo@parspack.com>
2023-01-04 21:57:16 +0330

    add the sample blade as default value

--- src/Messages/ValueObjects/MailMessage.php
+++ src/Messages/ValueObjects/MailMessage.php
@@ -6,7 +6,7 @@ use Illuminate\Mail\Mailable;
 
 class MailMessage extends Mailable implements NotifiableMessage
 {
-    public function __construct(public $subject, public $messageText ,public $view)
+    public function __construct(public $subject, public $messageText ,public $view = 'notification::emails.sample')
     {
         $this->subject($this->subject)
             ->view($this->view);
